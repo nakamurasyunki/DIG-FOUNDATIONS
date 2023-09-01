9eb44f23acc9243feaba40711aa9a4dcdf1cb972
nakamurasyunki <shiyunki_nakamura@mail.toyota.co.jp>
2023-09-01 13:04:29 +0900

    Day16

--- FOUNDATIONS/Day15/script.js
+++ FOUNDATIONS/Day15/script.js
@@ -5,9 +5,11 @@ console.log("--- 基礎演習 ---");
 console.log("--- 1 ---");
 
 console.log("--- 中級演習 ---");
+// const tagTitle = document.getElementsByClassName("title");
+
 // for (let i = 0; i < tagTitle.length; i++) {
 //   if (i % 2 === 1) {
-//       tagTitle[i].style.display = "none";
+//     tagTitle[i].style.display = "none";
 //   }
 // }
 console.log("--- 応用演習 ---");
